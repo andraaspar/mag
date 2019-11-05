30e328650d0ba536d9dc77eda93b270a733cba21
andraaspar <andraaspar@gmail.com>
2019-11-05 18:38:18 +0100

    Fix shuffle.

--- src/function/shuffle.tsx
+++ src/function/shuffle.tsx
@@ -1,7 +1,7 @@
 export function shuffle<T>(a: readonly T[]): T[] {
 	const result: T[] = []
 	for (const item of a) {
-		const newIndex = Math.floor(Math.random() * result.length + 1)
+		const newIndex = Math.floor(Math.random() * (result.length + 1))
 		result.splice(newIndex, 0, item)
 	}
 	return result
