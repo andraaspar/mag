cca3ad9708258a51c5bd2a11257f898535f3b55d
andraaspar <andraaspar@gmail.com>
2019-11-07 17:47:06 +0100

    Default styles.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
 import { AppComp } from './comp/AppComp'
+import './index.css'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
