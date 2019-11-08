6a71f3b402297695e22fae0f3972eff85b337bb2
andraaspar <andraaspar@gmail.com>
2019-11-08 19:29:26 +0100

    Updates

---
+++ precache-manifest.1b134e67e8c65a450494e313c9651289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde13bf82c61309430979a9ca770c843",
+    "url": "/mag/index.html"
+  },
+  {
+    "revision": "99e688beaea90a0a36e5",
+    "url": "/mag/static/css/2.12396101.chunk.css"
+  },
+  {
+    "revision": "6ffde36fe1f52ddf2b51",
+    "url": "/mag/static/css/main.ca057cc6.chunk.css"
+  },
+  {
+    "revision": "99e688beaea90a0a36e5",
+    "url": "/mag/static/js/2.da97c5ef.chunk.js"
+  },
+  {
+    "revision": "6ffde36fe1f52ddf2b51",
+    "url": "/mag/static/js/main.bb6e44fb.chunk.js"
+  },
+  {
+    "revision": "b77ac9a55cd32c0beb1c",
+    "url": "/mag/static/js/runtime-main.65d8cfb6.js"
+  }
+]);
