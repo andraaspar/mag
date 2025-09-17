209204e91c3b63324b10fef48775060903387628
András Parditka <andraaspar@gmail.com>
2025-09-17 13:56:38 +0200

    Fix testing.

--- src/function/avoidDuplicates.test.tsx
+++ src/function/avoidDuplicates.test.tsx
@@ -1,5 +1,4 @@
-import { equal, notEqual } from 'node:assert/strict'
-import { it } from 'node:test'
+import { expect, it } from 'vitest'
 import { avoidDuplicates } from './avoidDuplicates'
 
 export function equals(a: number, b: number) {
@@ -7,20 +6,20 @@ export function equals(a: number, b: number) {
 }
 
 it(`[q0icmo]`, () => {
-	equal(avoidDuplicates([0, 0, 1], equals), [0, 1, 0])
+	expect(avoidDuplicates([0, 0, 1], equals)).toEqual([0, 1, 0])
 })
 
 it(`[q0id5i]`, () => {
-	equal(avoidDuplicates([1, 0, 0], equals), [0, 1, 0])
+	expect(avoidDuplicates([1, 0, 0], equals)).toEqual([0, 1, 0])
 })
 
 it(`[q0idlk]`, () => {
 	const result = avoidDuplicates([0, 1, 2, 0], equals)
 	console.log(result)
-	notEqual(result[0], result[3])
-	equal(result.length, 4)
-	equal(result.filter((n) => n != null).length, 4)
-	equal(result.filter((n) => n !== 0).length, 2)
-	equal(result.includes(1), true)
-	equal(result.includes(2), true)
+	expect(result[0]).not.toBe(result[3])
+	expect(result.length).toBe(4)
+	expect(result.filter((n) => n != null).length).toBe(4)
+	expect(result.filter((n) => n !== 0).length).toBe(2)
+	expect(result.includes(1)).toBe(true)
+	expect(result.includes(2)).toBe(true)
 })
